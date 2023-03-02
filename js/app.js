92dde3c9f27375883f34359ead26657b6d54d87e
mehedihasan8 <mehedihasanfoysal22@gmail.com>
2023-03-02 21:32:18 +0600

    api fetch done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-console.log("okkay");
+const loadAllData = () => {
+  fetch("https://openapi.programming-hero.com/api/ai/tools")
+    .then((res) => res.json())
+    .then((data) => displayAllData(data.data.tools))
+    .catch((erorr) => {
+      console.log(erorr);
+    });
+};
+
+const displayAllData = (data) => {
+  const dataShowContainer = document.getElementById("show-all-conainer");
+  data.forEach((datas) => {
+    console.log(datas);
+  });
+  // console.log(data);
+};
